bad6860561f40e1d51445cf20b86a2c43762c31b
Jose Jesus Ochoa Torres <jose.jesus.ochoa@wizeline.com>
2022-08-04 16:49:35 -0500

    ci(vscode): vscode settings and tailwind eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ['plugin:react/recommended', 'standard', 'prettier'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react', '@typescript-eslint'],
-	rules: {},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier', "plugin:tailwindcss/recommended"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'tailwindcss'],
+  rules: {},
 }
